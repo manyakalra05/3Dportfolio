0af97ea2bd8140df8701c3da30d932746ccf3b4f
manyakalra05 <146841072+manyakalra05@users.noreply.github.com>
2025-06-11 10:43:38 +0530

    Add files via upload

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+
+import { arrow } from "../assets/icons";
+
+const HomeInfo = ({ currentStage }) => {
+  if (currentStage === 1)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Hi, I'm
+        <span className='font-semibold mx-2 text-white'>Manya Kalra</span>
+        👋
+        <br />
+        A BTech student from Indira Gandhi Delhi Technical University for Women.
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+          Working on enhancing my development skills, <br /> and learning new technologies!
+        </p>
+
+        <Link to='/about' className='neo-brutalism-white neo-btn'>
+          Learn more
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium text-center sm:text-xl'>
+          Created multiple projects in the last few months <br /> Curious to see them?
+        </p>
+
+        <Link to='/projects' className='neo-brutalism-white neo-btn'>
+          Visit my portfolio
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>
+        Let's dive in the world of development <br/> and create some fantastic projects!
+      </p>
+
+      <Link to='/contact' className='neo-brutalism-white neo-btn'>
+        Let's Connect!
+        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+      </Link>
+    </div>
+    );
+  }
+
+  return null;
+};
+
+export default HomeInfo;
