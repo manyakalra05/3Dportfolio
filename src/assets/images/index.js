d846704e232387f92f1a9d7e2c3136c50571e14d
manyakalra05 <146841072+manyakalra05@users.noreply.github.com>
2025-06-11 18:33:47 +0530

    Update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,15 +1,10 @@
-import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
-import starbucks from './starbucks.png'
-import tesla from './tesla.png'
+import igdtuw from './igdtuw.jpeg'
+import max from './max.png'
 import logo from './logo.png'
 
 export {
-    hero,
-    meta,
-    shopify,
-    starbucks,
-    tesla,
+    
+    igdtuw,
+    max,
     logo
 }
